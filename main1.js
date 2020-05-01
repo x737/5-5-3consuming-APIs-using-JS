7beb83785c672b86bea22fa6f7e7ffa29cdd814b
x737 <han9yolanda@gmail.com>
2020-05-01 21:40:18 +0000

    main1

---
+++ main1.js
@@ -0,0 +1,13 @@
+var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        data = this.responseText;
+    };
+}
+
+console.log(data);
